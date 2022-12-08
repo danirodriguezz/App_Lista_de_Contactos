8306a5a8685cc4f5a8577f833a35792d99e26867
Daniel Rodriguez <danielrodriguezalonso02@gmail.com>
2022-12-08 11:37:05 +0000

    Add register.php and create table user

--- sql/setup.sql
+++ sql/setup.sql
@@ -4,6 +4,15 @@ create database contacts_app;
 
 use contacts_app;
 
+create table users (
+    id int auto_increment primary key,
+    name varchar(255),
+    email varchar(255),
+    password varchar(255)
+);
+
+
+
 create table contacts (
     id int auto_increment primary key,
     name varchar(255),
@@ -11,3 +20,7 @@ create table contacts (
 );
 
 insert into contacts (name, phone_number) values ("Pepe", "1231232312");
+
+insert into users (name, email, password) values ("test", "test@gmail.com", "1234");
+
+
